9a547a238386ca22480b9a34d6227a15af13d48f
ImadMehmood535 <imadmehmood816@gmail.com>
2024-04-05 11:25:10 +0500

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import { Header_logo } from "@/assets";
 import Image from "next/image";
 import { useState } from "react";
-import { Link } from "react-scroll";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { FiShoppingCart } from "react-icons/fi";
 import { FaRegHeart } from "react-icons/fa";
 import { FiUser } from "react-icons/fi";
 import Searcharea from "./searchArea";
+import Link from "next/link";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -33,12 +33,7 @@ const Header = () => {
             <div className="flex flex-row items-center">
               <div className="basis-2/4 md:basis-1/6 ">
                 <div className=" max-w-[80%]">
-                  <Link
-                    to="home"
-                    spy={true}
-                    smooth={true}
-                    className="cursor-pointer"
-                  >
+                  <Link href="/" className="cursor-pointer">
                     <Image
                       src={Header_logo}
                       alt="Header_logo"
@@ -93,7 +88,6 @@ const Header = () => {
                         )}
                       </button>
                     </div>{" "}
-                   
                   </li>
                   <li className="relative">
                     <FiShoppingCart className="text-2xl" />{" "}
@@ -113,23 +107,17 @@ const Header = () => {
 
             <div className="hidden lg:block mb-3 cursor-pointer "></div>
           </div>
-          <div className="container relative  w-full  mx-auto ">
+          <div className={`    container relative  w-full  mx-auto `}>
             <ul
               className={`lg:flex lg:flex-row flex-col lg:h-full h-[100vh] lg:pt-0 pt-16  items-start text-white${
                 isMenuOpen ? "flex" : "hidden"
-              } font-medium lg:max-w-[500px] max-w-[300px] text-center pt-20 w-full lg:space-x-4 lg:space-y-0 space-y-4 lg:justify-end items-center xl:justify-start lg:items-center lg:static  top-0 right-0 text-black md:text-black bg-white/90 lg:bg-transparent `}
+              } font-medium lg:max-w-[500px] max-w-[300px] text-center pt-20 w-full lg:space-x-4 lg:space-y-0 space-y-4 lg:justify-end items-center xl:justify-start lg:items-center lg:static  top-0 right-0 text-black md:text-black   lg:bg-transparent `}
             >
               <li className="max-w-[300px] w-full">
                 <Link
-                  to="home"
-                  spy={true}
-                  smooth={true}
-                  className={`cursor-pointer opacity-100 hover:text-webRed-0 text-white${
-                    activeLink === "home"
-                      ? "font-extrabold text-themeSecondry-0"
-                      : ""
-                  }`}
-                  onSetActive={() => setActiveLink("home")}
+                  href="/"
+                  className="text-white"
+                  onSetActive={() => setActiveLink("/")}
                 >
                   Home
                 </Link>
@@ -137,14 +125,8 @@ const Header = () => {
 
               <li className="max-w-[300px] w-full">
                 <Link
-                  to="features"
-                  spy={true}
-                  smooth={true}
-                  className={`cursor-pointer opacity-100 hover:text-webRed-0 text-white${
-                    activeLink === "features"
-                      ? "font-extrabold text-themeSecondry-0"
-                      : ""
-                  }`}
+                  href="/sale"
+                  className="text-white"
                   onSetActive={() => setActiveLink("features")}
                 >
                   Sale
@@ -152,55 +134,30 @@ const Header = () => {
               </li>
               <li className="max-w-[300px] w-full relative group">
                 <Link
-                  to="aboutUs"
-                  spy={true}
-                  smooth={true}
-                  className={`cursor-pointer opacity-100 hover:text-webRed-0 text-white${
-                    activeLink === "aboutUs"
-                      ? "font-extrabold text-themeSecondry-0"
-                      : ""
-                  }`}
+                  href="/"
+                  className="text-white"
                   onSetActive={() => setActiveLink("pricing")}
                 >
-                  {/* <div className="flex gap-2 items-center justify-center">
-                      <span> About us </span>
-                    </div> */}
                   Shop
                 </Link>
               </li>
               <li className="max-w-[300px] w-full">
                 <Link
-                  to="pricing"
-                  spy={true}
-                  smooth={true}
-                  className={`cursor-pointer opacity-100 hover:text-webRed-0 text-white${
-                    activeLink === "pricing" ? " text-themeSecondry-0" : ""
-                  }`}
+                  href="/"
+                  className="text-white"
                   onSetActive={() => setActiveLink("pricing")}
                 >
                   Category
                 </Link>
-                {activeLink === "pricing" && (
-                  <hr className="border-primaryColor-0 w-[30%] mx-auto  " />
-                )}
-                <div className="block lg:hidden py-20 cursor-pointer "></div>
               </li>
               <li className="max-w-[300px] w-full">
                 <Link
-                  to="pricing"
-                  spy={true}
-                  smooth={true}
-                  className={`cursor-pointer opacity-100 hover:text-webRed-0 text-white${
-                    activeLink === "pricing" ? " text-themeSecondry-0" : ""
-                  }`}
+                  href="/blogs"
+                  className="text-white"
                   onSetActive={() => setActiveLink("pricing")}
                 >
-                  New
+                  Blog
                 </Link>
-                {activeLink === "pricing" && (
-                  <hr className="border-primaryColor-0 w-[30%] mx-auto  " />
-                )}
-                <div className="block lg:hidden py-20 cursor-pointer "></div>
               </li>
             </ul>
           </div>

f8eae95504b59b4b57c2f80e669c78ccfc6b9066
zeeshansheikh214 <wm.inspyre@gmail.com>
2024-04-05 12:02:33 +0500

    create product  cart component

---
+++ src/app/cart/page.js
@@ -0,0 +1,14 @@
+import CartDetails from '@/components/CartDetails'
+import InnerpageHeader from '@/components/InnerpageHeader'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='cart'>
+      {/* <InnerpageHeader pageTitle={"Shopping Cart"} breadcrums={"Account"} />
+      <CartDetails/> */}
+    </div>
+  )
+}
+
+export default page
--- src/components/Productarea.jsx
+++ src/components/Productarea.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { FaRegHeart } from "react-icons/fa";
-import { FiEye } from "react-icons/fi";
-import { FaExchangeAlt } from "react-icons/fa";
-import Image from "next/image";
-import Link from "next/link";
-
+import ProductCart from "./ProductCart";
 
 const Productarea = ({ data, option, isChecked, sorting }) => {
- 
-
   const filteredData = isChecked
     ? data.filter((item) => item?.sale_price)
     : data;
@@ -40,59 +33,7 @@ const Productarea = ({ data, option, isChecked, sorting }) => {
           }`}
         >
           {sortedData?.map((item, index) => (
-            <div className="item group/product" key={index}>
-              <div className="image-area relative border py-14 px-10 text-center min-h-[450px] overflow-hidden flex items-center">
-                <div className="tag-wrapper absolute top-2 left-2 ">
-                  <div className="bg-themeSecondry-0 uppercase rounded-md py-1 px-4 text-white font-bold mb-2">
-                    {item?.tags?.tag}
-                  </div>
-                  {item?.tags?.discount && (
-                    <div className="bg-themePrimary-0 uppercase rounded-md py-1 px-4 text-white font-bold">
-                      {item?.tags?.discount}
-                    </div>
-                  )}
-                </div>
-                <div className="options absolute top-4  -right-20 group-hover/product:right-2">
-                  <div className="options_list flex flex-wrap flex-col gap-4">
-                    <div className="rounded-lg  bg-white shadow-lg px-4 py-4 ">
-                      <FaRegHeart className="text-2xl" />
-                    </div>
-                    <div className="rounded-lg  bg-white shadow-lg px-4 py-4 ">
-                      <FiEye className="text-2xl cursor-pointer"  />
-                      
-                    </div>
-                    <div className="rounded-lg  bg-white shadow-lg px-4 py-4 ">
-                      <FaExchangeAlt className="text-2xl" />
-                    </div>
-                  </div>
-                </div>
-                <Image
-                  src={item?.image}
-                  alt={item?.image}
-                  width={250}
-                  height={250}
-                  className="w-full "
-                />
-                <div className="btn-area absolute -bottom-20 group-hover/product:bottom-0 left-0 right-0 py-3 px-2 bg-[#121212] text-white">
-                  <Link href="/" className="cursor-pointer">
-                    Quick Options
-                  </Link>
-                </div>
-              </div>
-              <div className="detail-area py-3">
-                <h4 className="font-bold text-[#121212] text-base capitalize">
-                  {item?.title}
-                </h4>
-                <div className="flex flex-wrap items-center gap-2 ">
-                  <p className="text-[#DB4444] font-medium uppercase">
-                    AED {item?.sale_price}
-                  </p>
-                  <span className="line-through text-[#A0A0A0] uppercase text-sm">
-                    AED {item?.reg_price}
-                  </span>
-                </div>
-              </div>
-            </div>
+            <ProductCart item={item} key={index} />
           ))}
         </div>
       </div>
